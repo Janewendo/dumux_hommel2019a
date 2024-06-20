6d46c9071eaac427f9998bf91ead62b8aed4c725
Jianwen Du <48660877+Janewendo@users.noreply.github.com>
2024-06-19 20:45:50 -0700

    Update leominsolids.hh based on enzymeminsolids.hh

--- dumux/material/solidsystems/leominsolids.hh
+++ dumux/material/solidsystems/leominsolids.hh
@@ -21,8 +21,8 @@
  * \ingroup SolidSystems
  * \brief @copybrief Dumux::SolidSystems::InertSolidPhase
  */
-#ifndef DUMUX_SOLIDSYSTEMS_EICP_SOLID_PHASE_HH
-#define DUMUX_SOLIDSYSTEMS_EICP_SOLID_PHASE_HH
+#ifndef DUMUX_SOLIDSYSTEMS_LEO_SOLID_PHASE_HH
+#define DUMUX_SOLIDSYSTEMS_LEO_SOLID_PHASE_HH
 
 #include <string>
 #include <dune/common/exceptions.hh>
@@ -30,6 +30,7 @@
 #include <dumux/material/components/urease.hh>
 #include <dumux/material/components/calcite.hh>
 #include <dumux/material/components/granite.hh>
+#include <dumux/material/components/ferrohydrite.hh>
 
 namespace Dumux {
 namespace SolidSystems {
@@ -42,22 +43,24 @@ namespace SolidSystems {
  * \note inert components have to come after all non-inert components
  */
 template <class Scalar>
-class EnzymeMinSolidPhase
+class LeoMinSolidPhase
 {
 public:
     using Jbme = Components::Urease<Scalar>;
     using Calcite = Components::Calcite<Scalar>;
     using Granite = Components::Granite<Scalar>;
-
+    using Ferrohydrite = Components::Ferrohydrite<Scalar>;
+	
     /****************************************
      * Solid phase related static parameters
      ****************************************/
-    static constexpr int numComponents = 3;
+    static constexpr int numComponents = 4;
     static constexpr int numInertComponents = 1;
     static constexpr int JbmeIdx = 0;
     static constexpr int CalciteIdx = 1;
     static constexpr int GraniteIdx = 2;
-
+    static constexpr int FerrohydriteIdx = 3;
+	
     /*!
      * \brief Return the human readable name of a solid phase
      *
@@ -70,6 +73,7 @@ public:
             case JbmeIdx: return Jbme::name();
             case CalciteIdx: return Calcite::name();
             case GraniteIdx: return Granite::name();
+            case FerrohydriteIdx: return Ferrohydrite::name();
             default: DUNE_THROW(Dune::InvalidStateException, "Invalid component index " << compIdx);
         }
     }
@@ -78,7 +82,7 @@ public:
      * \brief A human readable name for the solid system.
      */
     static std::string name()
-    { return "EnzymeMinSolidPhase"; }
+    { return "LeoMinSolidPhase"; }
 
     /*!
      * \brief Returns whether the phase is incompressible
@@ -102,6 +106,7 @@ public:
             case JbmeIdx: return Jbme::molarMass();
             case CalciteIdx: return Calcite::molarMass();
             case GraniteIdx: return Granite::molarMass();
+            case FerrohydriteIdx: return Ferrohydrite::molarMass();
             default: DUNE_THROW(Dune::InvalidStateException, "Invalid component index " << compIdx);
         }
     }
@@ -115,14 +120,17 @@ public:
         const Scalar rho1 = Jbme::solidDensity(solidState.temperature());
         const Scalar rho2 = Calcite::solidDensity(solidState.temperature());
         const Scalar rho3 = Granite::solidDensity(solidState.temperature());
+        const Scalar rho4 = Ferrohydrite::solidDensity(solidState.temperature());
         const Scalar volFrac1 = solidState.volumeFraction(JbmeIdx);
         const Scalar volFrac2 = solidState.volumeFraction(CalciteIdx);
         const Scalar volFrac3 = solidState.volumeFraction(GraniteIdx);
+        const Scalar volFrac4 = solidState.volumeFraction(FerrohydriteIdx);
 
         return (rho1*volFrac1+
                 rho2*volFrac2+
-                rho3*volFrac3)
-               /(volFrac1+volFrac2+volFrac3);
+                rho3*volFrac3+
+                rho4*volFrac4)
+               /(volFrac1+volFrac2+volFrac3+volFrac4);
     }
 
     /*!
@@ -136,6 +144,7 @@ public:
             case JbmeIdx: return Jbme::solidDensity(solidState.temperature());
             case CalciteIdx: return Calcite::solidDensity(solidState.temperature());
             case GraniteIdx: return Granite::solidDensity(solidState.temperature());
+            case FerrohydriteIdx: return Ferrohydrite::solidDensity(solidState.temperature());
             default: DUNE_THROW(Dune::InvalidStateException, "Invalid component index " << compIdx);
         }
     }
@@ -151,6 +160,7 @@ public:
             case JbmeIdx: return Jbme::solidDensity(solidState.temperature())/Jbme::molarMass();
             case CalciteIdx: return Calcite::solidDensity(solidState.temperature())/Calcite::molarMass();
             case GraniteIdx: return Granite::solidDensity(solidState.temperature())/Granite::molarMass();
+            case FerrohydriteIdx: return Ferrohydrite::solidDensity(solidState.temperature())/Ferrohydrite::molarMass();
             default: DUNE_THROW(Dune::InvalidStateException, "Invalid component index " << compIdx);
         }
     }
@@ -164,14 +174,17 @@ public:
         const Scalar lambda1 = Jbme::solidThermalConductivity(solidState.temperature());
         const Scalar lambda2 = Calcite::solidThermalConductivity(solidState.temperature());
         const Scalar lambda3 = Granite::solidThermalConductivity(solidState.temperature());
+        const Scalar lambda4 = Ferrohydrite::solidThermalConductivity(solidState.temperature());
         const Scalar volFrac1 = solidState.volumeFraction(JbmeIdx);
         const Scalar volFrac2 = solidState.volumeFraction(CalciteIdx);
         const Scalar volFrac3 = solidState.volumeFraction(GraniteIdx);
-
+        const Scalar volFrac4 = solidState.volumeFraction(FerrohydriteIdx);
+		
         return (lambda1*volFrac1+
                 lambda2*volFrac2+
-                lambda3*volFrac3)
-               /(volFrac1+volFrac2+volFrac3);
+                lambda3*volFrac3+
+                lambda4*volFrac4)				
+               /(volFrac1+volFrac2+volFrac3+volFrac4);
     }
 
     /*!
@@ -183,14 +196,17 @@ public:
         const Scalar c1 = Jbme::solidHeatCapacity(solidState.temperature());
         const Scalar c2 = Calcite::solidHeatCapacity(solidState.temperature());
         const Scalar c3 = Granite::solidHeatCapacity(solidState.temperature());
+        const Scalar c4 = Ferrohydrite::solidHeatCapacity(solidState.temperature());
         const Scalar volFrac1 = solidState.volumeFraction(JbmeIdx);
         const Scalar volFrac2 = solidState.volumeFraction(CalciteIdx);
         const Scalar volFrac3 = solidState.volumeFraction(GraniteIdx);
-
+        const Scalar volFrac4 = solidState.volumeFraction(FerrohydriteIdx);
+		
         return (c1*volFrac1+
                 c2*volFrac2+
-                c3*volFrac3)
-               /(volFrac1+volFrac2+volFrac3);
+                c3*volFrac3+
+                c4*volFrac4)
+               /(volFrac1+volFrac2+volFrac3+volFrac4);
     }
 
 };
@@ -199,3 +215,4 @@ public:
 } // end namespace Dumux
 
 #endif
+
